8a78654026b4300ea30df2a386968d0e4f0bbbc4
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-12-19 11:53:40 +0100

    chore: add dummy components for views

---
+++ src/views/index.ts
@@ -0,0 +1,5 @@
+export * from './home/home.component';
+export * from './about/about.component';
+export * from './gallery/gallery.component';
+export * from './pricing/pricing.component';
+export * from './contact/contact.component';
---
+++ src/views/pricing/pricing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mdlv-pricing',
+  imports: [],
+  templateUrl: './pricing.component.html',
+  styleUrl: './pricing.component.scss'
+})
+export class PricingComponent {
+
+}
