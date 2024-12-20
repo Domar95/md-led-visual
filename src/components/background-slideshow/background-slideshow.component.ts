c63193abb80fd84fdfc3bf30b5de52395e061dff
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-12-20 02:16:21 +0100

    feat: add animated text to the slideshow

--- src/components/background-slideshow/background-slideshow.component.ts
+++ src/components/background-slideshow/background-slideshow.component.ts
@@ -1,25 +1,52 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { BackgroundSlideshowImage } from '../../models/background-slideshow.model';
 
 @Component({
   selector: 'mdlv-background-slideshow',
   imports: [CommonModule],
   templateUrl: './background-slideshow.component.html',
   styleUrl: './background-slideshow.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0 })),
+      transition('void => *', [animate('1s ease-in')]),
+    ]),
+  ],
 })
 export class BackgroundSlideshowComponent implements OnInit, OnDestroy {
-  images: string[] = [
-    'assets/images/homepage_1.jpg',
-    'assets/images/homepage_2.jpg',
+  images: BackgroundSlideshowImage[] = [
+    {
+      uri: 'assets/images/homepage_1.jpg',
+      text: 'PROFESJONALNA OBSŁUGA IMPREZ',
+    },
+    {
+      uri: 'assets/images/homepage_2.jpg',
+      text: 'WYNAJEM EKRANÓW LED I TELEBIMÓW',
+    },
   ];
+
   currentImageIndex: number = 0;
   intervalId!: ReturnType<typeof setInterval>;
+  showText: boolean = false;
 
   ngOnInit() {
     this.intervalId = setInterval(() => {
       this.currentImageIndex =
         (this.currentImageIndex + 1) % this.images.length;
-    }, 5000);
+      this.showText = false;
+      setTimeout(() => {
+        this.showText = true;
+      }, 2000);
+    }, 7000);
   }
 
   ngOnDestroy() {
