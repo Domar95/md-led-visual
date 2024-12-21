4e4506fc65cf831a876ace8fd06527ba5505d695
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-12-21 17:58:38 +0100

    fix(layout): resolve issue with missing child routes content

--- src/layouts/default-layout/default-layout.component.ts
+++ src/layouts/default-layout/default-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { NavbarComponent, TitleComponent } from '@components/index';
 
 @Component({
   selector: 'mdlv-default-layout',
-  imports: [NavbarComponent, TitleComponent],
+  imports: [RouterModule, NavbarComponent, TitleComponent],
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.scss',
 })
--- src/layouts/home-layout/home-layout.component.ts
+++ src/layouts/home-layout/home-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from '@components/index';
 
 @Component({
   selector: 'mdlv-home-layout',
-  imports: [NavbarComponent],
+  imports: [RouterModule, NavbarComponent],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.scss',
 })
