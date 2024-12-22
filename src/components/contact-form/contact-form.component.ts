6edc4f229908576aff35ff04d2bfa7264472ed0f
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-12-22 16:45:20 +0100

    chore: generate component for contact form

---
+++ src/components/contact-form/contact-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mdlv-contact-form',
+  imports: [],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './background-slideshow/background-slideshow.component';
 export * from './learn-more/learn-more.component';
 export * from './navbar/navbar.component';
 export * from './title/title.component';
+export * from './contact-form/contact-form.component';
