8f1f8b4787b5d81f4c6eee7cea46fb6a2307cd0e
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-12-27 20:37:37 +0100

    feat: add offer banner component

---
+++ src/components/offer-banner/offer-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mdlv-offer-banner',
+  imports: [],
+  templateUrl: './offer-banner.component.html',
+  styleUrl: './offer-banner.component.scss'
+})
+export class OfferBannerComponent {
+
+}
