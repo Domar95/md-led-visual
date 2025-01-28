4ce672a8027d6c1ae82df81e4bd88c5731700d2b
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-01-28 17:55:09 +0100

    chore: add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyBdVToWbccXWBfWfA5iS4iEadugD-VNhNM',
+    authDomain: 'md-led-visual.firebaseapp.com',
+    projectId: 'md-led-visual',
+    storageBucket: 'md-led-visual.firebasestorage.app',
+    messagingSenderId: '654372960384',
+    appId: '1:654372960384:web:198c257539512bf3470e65',
+    measurementId: 'G-EQMM08X1SE',
+  },
+};
