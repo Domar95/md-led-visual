8929567dc32cc3797fe103828fb0f8d5ebdcec79
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-01-29 22:17:18 +0100

    chore(firebase): add firebase app provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,10 @@ import {
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 import { routes } from './app.routes';
+import { environment } from 'src/environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptorsFromDi()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
   ],
 };
