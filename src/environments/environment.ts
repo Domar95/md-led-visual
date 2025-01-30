2dc48198b882a62df01b8f6f1139d70bea81f914
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-01-30 01:45:42 +0100

    chore: temporarily use dev storage instance in prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     apiKey: 'AIzaSyBdVToWbccXWBfWfA5iS4iEadugD-VNhNM',
     authDomain: 'md-led-visual.firebaseapp.com',
     projectId: 'md-led-visual',
-    storageBucket: 'md-led-visual.firebasestorage.app',
+    storageBucket: 'md-led-visual-dev', // TODO: temporarily use dev; update once perm/auth is set up and images are added to prod: md-led-visual.firebasestorage.app
     messagingSenderId: '654372960384',
     appId: '1:654372960384:web:198c257539512bf3470e65',
     measurementId: 'G-EQMM08X1SE',
