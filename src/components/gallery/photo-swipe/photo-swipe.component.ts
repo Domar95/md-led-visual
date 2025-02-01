051e135066c4f9a6bbed4aacdfe53d9cb8b4aafc
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-02-01 20:34:49 +0100

    chore(photo-swipe): clean up redundant classes & id

--- src/components/gallery/photo-swipe/photo-swipe.component.ts
+++ src/components/gallery/photo-swipe/photo-swipe.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
 import PhotoSwipe from 'photoswipe';
-import { MatGridListModule } from '@angular/material/grid-list';
 
 import { GalleryImage } from '@models/gallery.model';
 
@@ -19,7 +19,7 @@ export class PhotoSwipeComponent {
   ngOnInit() {
     this.photoSwipeLightbox = new PhotoSwipeLightbox({
       // may select multiple "galleries"
-      gallery: '#gallery--getting-started',
+      gallery: '#photoswipe-gallery',
 
       // Elements within gallery (slides)
       children: 'a',
