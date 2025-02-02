77fa963dcdfc51e8052052b411088d4b5d71a592
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-02-02 16:18:08 +0100

    style(contact): use flexbox instead of mat grids

--- src/components/contact/contact.component.ts
+++ src/components/contact/contact.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
 
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
@@ -8,8 +6,6 @@ import { ContactInfoComponent } from './contact-info/contact-info.component';
 @Component({
   selector: 'mdlv-contact',
   imports: [
-    MatCardModule,
-    MatGridListModule,
     ContactFormComponent,
     ContactInfoComponent
   ],
