330ae927f0eeb6897adae64d87b1dee2ab13f7ce
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-02-02 01:05:32 +0100

    refactor(slideshow): extract slider to separate component

--- src/components/background-slideshow/background-slideshow.component.ts
+++ src/components/background-slideshow/background-slideshow.component.ts
@@ -1,90 +1,22 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
-import Swiper from 'swiper';
-import {
-  Autoplay,
-  EffectFade,
-  Keyboard,
-  Navigation,
-  Pagination,
-} from 'swiper/modules';
-import {
-  trigger,
-  state,
-  style,
-  transition,
-  animate,
-} from '@angular/animations';
 
 import { BackgroundSlideshowImage } from '@models/background-slideshow.model';
 import { FirebaseService } from '@services/firebase.service';
+import { SliderComponent } from './slider/slider.component';
 
 @Component({
   selector: 'mdlv-background-slideshow',
-  imports: [CommonModule, RouterModule, MatButtonModule],
+  imports: [SliderComponent],
   templateUrl: './background-slideshow.component.html',
   styleUrl: './background-slideshow.component.scss',
-  animations: [
-    trigger('fadeIn', [
-      state('void', style({ opacity: 0 })),
-      state('*', style({ opacity: 1 })),
-      transition('void => *', animate('2s ease-in')),
-    ]),
-    trigger('slideIn', [
-      state('void', style({ transform: 'translateY(100%)' })),
-      state('*', style({ transform: 'translateY(0)' })),
-      transition('void => *', animate('1s ease-out')),
-    ]),
-  ],
 })
 export class BackgroundSlideshowComponent implements OnInit {
-  swiper!: Swiper;
-  images: BackgroundSlideshowImage[] = [];
-  activeSlideIndex = 0;
+  images!: BackgroundSlideshowImage[];
 
   constructor(private firebaseService: FirebaseService) {}
 
   async ngOnInit() {
     this.images = await this.loadImages();
-
-    this.swiper = new Swiper('.heroSwiper', {
-      modules: [Navigation, Pagination, Autoplay, Keyboard, EffectFade],
-      effect: 'fade',
-      fadeEffect: {
-        crossFade: true,
-      },
-      speed: 1400,
-      centeredSlides: true,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-      },
-      on: {
-        slideChange: () => {
-          this.activeSlideIndex = this.swiper.realIndex;
-        },
-      },
-    });
-
-    setTimeout(() => {
-      if (this.swiper) {
-        this.swiper.update();
-      }
-    }, 0);
   }
 
   async loadImages(): Promise<BackgroundSlideshowImage[]> {
