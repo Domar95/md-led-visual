ccba0c888130847e358c4e7844225ac234814bec
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-02-02 12:41:41 +0100

    refactor(contact): extract contact info to separate component

---
+++ src/components/contact/contact-info/contact-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'mdlv-contact-info',
+  imports: [MatIconModule],
+  templateUrl: './contact-info.component.html',
+  styleUrl: './contact-info.component.scss'
+})
+export class ContactInfoComponent { }
--- src/components/contact/contact.component.ts
+++ src/components/contact/contact.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDividerModule } from '@angular/material/divider';
 
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 @Component({
   selector: 'mdlv-contact',
   imports: [
-    MatIconModule,
     MatCardModule,
     MatGridListModule,
-    MatDividerModule,
     ContactFormComponent,
+    ContactInfoComponent
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
