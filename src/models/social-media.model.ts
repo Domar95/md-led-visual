92036b39b2d24ebfa568d116028071a30f717745
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-03-09 20:31:15 +0100

    chore(social-media): add urls

--- src/models/social-media.model.ts
+++ src/models/social-media.model.ts
@@ -1,13 +1,13 @@
 export type SocialMedia = {
-  facebook: string,
-  youtube: string,
-  instagram: string
-}
+  facebook: string;
+  youtube: string;
+  instagram: string;
+};
 
-export type SocialMediaCategory = keyof SocialMedia
+export type SocialMediaCategory = keyof SocialMedia;
 
 export const SOCIAL_MEDIA_URLS: SocialMedia = {
-  facebook: 'http://facebook.com',
-  youtube: 'http://youtube.com',
-  instagram: 'http://instagram.com'
-}
+  facebook: 'http://facebook.com/profile.php?id=61555615919454',
+  youtube: 'http://youtube.com/@domaradzkimarcin',
+  instagram: 'http://instagram.com',
+};
